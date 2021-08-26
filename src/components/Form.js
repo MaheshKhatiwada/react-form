18923ed6ccdc48763d7cb4e7a158a5006903d90b
MaheshKhatiwada <maheshkhatiwada17@gmail.com>
2021-08-26 14:55:39 +0545

    added education and experience fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,16 +5,26 @@ const Form = () => {
     name: "",
     dob: "",
     email: "",
+    board:"",
+    college:"",
+    gpa:"",
+    company:"",
+    role:"",
+    years:"",
+    education:[],
+    experience:[],
   });
   const handleFormData = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
+
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    //education.push()
     console.log("Form data is ", formData);
   };
   return (
@@ -54,6 +64,45 @@ const Form = () => {
             required
           />
         </div>
+        <h6 className="mt-2"> Education Details</h6>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Board</th>
+              <th scope="col">College</th>
+              <th scope="col">GPA</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td><input type="text" name="board" value={formData.board} onChange={handleFormData} placeholder="Board" /></td>
+              <td><input type="text" name="college" value={formData.college} onChange={handleFormData} placeholder="College" /></td>
+              <td><input type="number" name="gpa" value={formData.gpa} onChange={handleFormData} placeholder="GPA" /></td>
+            </tr>
+          </tbody>
+        </table>
+        <h6 className="mt-2"> Experience</h6>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Company</th>
+              <th scope="col">Role</th>
+              <th scope="col">Years</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td><input type="text" name="company" value={formData.company} onChange={handleFormData} placeholder="Company" /></td>
+              <td><input type="text" name="role" value={formData.role} onChange={handleFormData} placeholder="Role" /></td>
+              <td><input type="number" name="years" value={formData.years} onChange={handleFormData} placeholder="Years" /></td>
+
+            </tr>
+          </tbody>
+        </table>
 
 
         <div className="mt-2">
