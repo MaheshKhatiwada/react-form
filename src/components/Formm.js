7309058c89800c14f9c9bd8cfa551aa250cec24a
MaheshKhatiwada <maheshkhatiwada17@gmail.com>
2021-08-27 15:51:57 +0545

    removed key prop error

--- src/components/Formm.js
+++ src/components/Formm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import isEmpty from "validator/lib/isEmpty";
 import isEmail from "validator/lib/isEmail";
 import isDate from "validator/lib/isDate";
@@ -14,7 +14,7 @@ const Formm = () => {
     experience: [{ company: "", role: "", years: "" }],
   });
   const [errors, setErrors] = useState("");
-  const { name, dob, email,education,experience} = data;
+  const { name, dob, email, education, experience } = data;
   const handleFormData = (e) => {
     setData({
       ...data,
@@ -22,29 +22,22 @@ const Formm = () => {
     });
     setErrors("");
   };
-  const handleFormEducationData=(e,idx)=>{
-   // console.log(idx,e.target.name)
-    const values=[...data.education];
-    values[idx][e.target.name]=e.target.value;
-    setData({...data,education:values})
-
-  }
-  const handleFormExperienceData=(e,idx)=>{
-
+  const handleFormEducationData = (e, idx) => {
+    // console.log(idx,e.target.name)
+    const values = [...data.education];
+    values[idx][e.target.name] = e.target.value;
+    setData({ ...data, education: values });
+  };
+  const handleFormExperienceData = (e, idx) => {
     //console.log(idx,e.target.name)
-    const values=[...data.experience];
-    values[idx][e.target.name]=e.target.value;
-    setData({...data,experience:values})
-  }
+    const values = [...data.experience];
+    values[idx][e.target.name] = e.target.value;
+    setData({ ...data, experience: values });
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    if (
-      isEmpty(name) ||
-      isEmpty(dob) ||
-      isEmpty(email)
-
-    ) {
+    if (isEmpty(name) || isEmpty(dob) || isEmpty(email)) {
       setErrors("All fields required");
     } else if (!isEmail(email)) {
       setErrors("Email must be valid");
@@ -56,36 +49,41 @@ const Formm = () => {
       console.log("Data is ", data);
     }
   };
-  const handleAddEducation=()=>{
-      setData({...data,education:[...education,{ board: "", college: "", gpa: "" }]})
-  }
-  const handleAddExperience=()=>{
-      setData({...data,experience:[...experience,{ company: "", role: "", years: "" }]})
-  }
-  const handleDeleteEducation=(idx)=>{
+  const handleAddEducation = () => {
+    setData({
+      ...data,
+      education: [...education, { board: "", college: "", gpa: "" }],
+    });
+  };
+  const handleAddExperience = () => {
+    setData({
+      ...data,
+      experience: [...experience, { company: "", role: "", years: "" }],
+    });
+  };
+  const handleDeleteEducation = (idx) => {
     //  console.log(idx)
-     const values=[...data.education];
-     values.splice(idx,1)
+    const values = [...data.education];
+    values.splice(idx, 1);
     // console.log(values);
     // console.log(data.education)
-    setData({...data,education:values})
-
-  }
-  const handleDeleteExperience=(idx)=>{
-    const values=[...data.experience];
-    values.splice(idx,1)
-    setData({...data,experience:values})
-  }
-  const handleClear=()=>{
-      setData({
-        name: "",
-        dob: "",
-        email: "",
-        education: [{ board: "", college: "", gpa: "" }],
-        experience: [{ company: "", role: "", years: "" }],
-      })
-      setErrors('')
-  }
+    setData({ ...data, education: values });
+  };
+  const handleDeleteExperience = (idx) => {
+    const values = [...data.experience];
+    values.splice(idx, 1);
+    setData({ ...data, experience: values });
+  };
+  const handleClear = () => {
+    setData({
+      name: "",
+      dob: "",
+      email: "",
+      education: [{ board: "", college: "", gpa: "" }],
+      experience: [{ company: "", role: "", years: "" }],
+    });
+    setErrors("");
+  };
   return (
     <div>
       <form onSubmit={handleFormSubmit} noValidate>
@@ -125,7 +123,71 @@ const Formm = () => {
           />
         </div>
         <h6 className="mt-2"> Education Details</h6>
-        <button className="btn btn-secondary" onClick={handleAddEducation}>Add </button>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="gender"
+            id="male"
+          />
+          <label className="form-check-label">Male</label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="gender"
+            id="female"
+          />
+          <label className="form-check-label">Female</label>
+        </div>
+
+
+        <h6 className="mt-2">Languages</h6>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+          />
+          <label className="form-check-label">
+            Nepali
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+          />
+          <label className="form-check-label">
+            Indian
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+          />
+          <label className="form-check-label">
+            Chinese
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+          />
+          <label className="form-check-label">
+            English
+          </label>
+        </div>
+        <h6 className="mt-2"> Education Details</h6>
+        <button className="btn btn-secondary" onClick={handleAddEducation}>
+          Add{" "}
+        </button>
         <table className="table">
           <thead>
             <tr>
@@ -136,48 +198,56 @@ const Formm = () => {
             </tr>
           </thead>
           <tbody>
-                {data.education.map((eds,idx)=>{
-                    return(
-                        <>
-                        <tr key={idx}>
-                        <th scope="row"><button className="btn btn-secondary" onClick={()=>handleDeleteEducation(idx)}>Delete </button></th>
+            {data.education.map((eds, idx) => {
+              return (
+                <Fragment key={idx}>
+                  <tr >
+                    <th scope="row">
+                      <button
+                        className="btn btn-secondary"
+                        onClick={() => handleDeleteEducation(idx)}
+                      >
+                        Delete{" "}
+                      </button>
+                    </th>
 
-                        <td>
-                        <input
-                          type="text"
-                          name="board"
-                          value={eds.board}
-                          onChange={(e)=>handleFormEducationData(e,idx)}
-                          placeholder="Board"
-                        />
-                      </td>
-                      <td>
-
-                <input
-                  type="text"
-                  name="college"
-                  value={eds.college}
-                  onChange={(e)=>handleFormEducationData(e,idx)}
-                  placeholder="College"
-                />
-              </td>
-              <td>
-                <input
-                  type="number"
-                  name="gpa"
-                  value={eds.gpa}
-                  onChange={(e)=>handleFormEducationData(e,idx)}
-                  placeholder="GPA"
-                />
-              </td>
-              </tr>
-                </>
-                    )
-                })}
+                    <td>
+                      <input
+                        type="text"
+                        name="board"
+                        value={eds.board}
+                        onChange={(e) => handleFormEducationData(e, idx)}
+                        placeholder="Board"
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="college"
+                        value={eds.college}
+                        onChange={(e) => handleFormEducationData(e, idx)}
+                        placeholder="College"
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="number"
+                        name="gpa"
+                        value={eds.gpa}
+                        onChange={(e) => handleFormEducationData(e, idx)}
+                        placeholder="GPA"
+                      />
+                    </td>
+                  </tr>
+                </Fragment>
+              );
+            })}
           </tbody>
         </table>
         <h6 className="mt-2"> Experience</h6>
-        <button className="btn btn-secondary" onClick={handleAddExperience}>Add </button>
+        <button className="btn btn-secondary" onClick={handleAddExperience}>
+          Add{" "}
+        </button>
 
         <table className="table">
           <thead>
@@ -189,51 +259,61 @@ const Formm = () => {
             </tr>
           </thead>
           <tbody>
-          {data.experience.map((exp,idx)=>{
-                    return(
-                        <>
-                        <tr key={idx}>
-                        <th scope="row"><button className="btn btn-secondary" onClick={handleDeleteExperience}>Delete </button></th>
-                        <td>
-                        <input
-                          type="text"
-                          name="company"
-                          value={exp.company}
-                          onChange={(e)=>handleFormExperienceData(e,idx)}
-                          placeholder="Company"
-                        />
-                      </td>
-                      <td>
-
-                <input
-                  type="text"
-                  name="role"
-                  value={exp.role}
-                  onChange={(e)=>handleFormExperienceData(e,idx)}
-                  placeholder="Role"
-                />
-              </td>
-              <td>
-                <input
-                  type="number"
-                  name="years"
-                  value={exp.years}
-                  onChange={(e)=>handleFormExperienceData(e,idx)}
-                  placeholder="years"
-                />
-              </td>
-              </tr>
-                </>
-                    )
-                })}
+            {data.experience.map((exp, idx) => {
+              return (
+                <Fragment key={idx}>
+                  <tr>
+                    <th scope="row">
+                      <button
+                        className="btn btn-secondary"
+                        onClick={handleDeleteExperience}
+                      >
+                        Delete{" "}
+                      </button>
+                    </th>
+                    <td>
+                      <input
+                        type="text"
+                        name="company"
+                        value={exp.company}
+                        onChange={(e) => handleFormExperienceData(e, idx)}
+                        placeholder="Company"
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="role"
+                        value={exp.role}
+                        onChange={(e) => handleFormExperienceData(e, idx)}
+                        placeholder="Role"
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="number"
+                        name="years"
+                        value={exp.years}
+                        onChange={(e) => handleFormExperienceData(e, idx)}
+                        placeholder="years"
+                      />
+                    </td>
+                  </tr>
+                </Fragment>
+              );
+            })}
           </tbody>
         </table>
 
         <div className="mt-2">
-          <button type="submit" className="btn btn-primary" onClick={handleFormSubmit}>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleFormSubmit}
+          >
             Submit
           </button>
-          <button  className="btn btn-primary px-6" onClick={handleClear}>
+          <button className="btn btn-primary px-6" onClick={handleClear}>
             Clear
           </button>
         </div>
